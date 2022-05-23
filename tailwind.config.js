22a9c54c8d14c6f7aaadf1b9c301e1ec00e593e1
Yuseph Maulana <maulanayuseph@gmail.com>
2022-05-23 08:55:41 +0700

    refactor (tailwind.config.js): add config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,41 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
 module.exports = {
-  theme: {},
+  theme: {
+    theme: {
+      screens: {
+        'xs': '280px'
+      }
+    },
+    extend: {
+      colors: {
+        'brand-green-lighter': '#8FC749',
+        'brand-green-light': '#76B74B',
+        'brand-green': '#5AAA4E',
+        'brand-green-dark': '#399F4F',
+        'brand-green-darker': '#069550',
+        'brand-green-darkest': '#008C50',
+        'brand-blue-lighter': '#01BEF2',
+        'brand-blue-light': '#00B0E9',
+        'brand-blue': '#00A5E1',
+        'brand-blue-dark': '#009BDA',
+        'brand-blue-darker': '#0090D3',
+        'brand-blue-darkest': '#0087CB',
+        'brand-yellow-lighter': '#FFF212',
+        'brand-yellow-light': '#FFF212',
+        'brand-yellow': '#FFE11E',
+        'brand-yellow-dark': '#FFD126',
+        'brand-yellow-darker': '#FFD126',
+        'brand-yellow-darkest': '#FEC32B',
+        'brand-black': '#424242',
+        'brand-gray': '#FAFAFA',
+        'brand-gray-dark': '#757575'
+      }
+    },
+    fontFamily: {
+      'roboto': 'Roboto, sans-serif',
+      'lora': 'Lora, serif',
+      'lato': 'Lato, serif'
+    }
+  },
   variants: {},
   plugins: []
 }
